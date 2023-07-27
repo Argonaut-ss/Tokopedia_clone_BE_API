b37d96d9c88f31cf95446c87aaf99df42e691489
Argonaut <samysyafta@gmail.com>
2023-07-28 01:38:16 +0700

    Update Routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import swaggerRoute from "../config/swagger.js";
-import apiRoutes from "./api/index.js";
+import apiRoutes from "./api/api-routes.js";
 
 const router = express.Router();
 
+//API Documentation routes
 router.use("/docs", swaggerRoute);
+//routes for diffrent API
 router.use("/api", apiRoutes);
 
 export default router;
