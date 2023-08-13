e4e7db2a010da50a8cd7f0ab6aec67deb02157fd
Argonaut <samysyafta@gmail.com>
2023-08-13 16:45:58 +0700

    feat: add validation

--- src/routes/api/product-api-routes.js
+++ src/routes/api/product-api-routes.js
@@ -4,6 +4,6 @@ import ProductController from "../../controllers/product-controller.js";
 const productRouter = express.Router();
 
 productRouter.get("/:videoId", ProductController.getAllProductsByVideoId);
-productRouter.post("/:videoId", ProductController.addProduct);
+productRouter.post("/:video_id", ProductController.addProduct);
 
 export default productRouter;
